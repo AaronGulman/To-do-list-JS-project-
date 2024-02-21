afd4ca6ff36bc9547857f933ea6150d70cd69e24
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-21 12:02:17 -0500

    Update webpack.congif.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,71 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-const path = require('path');
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
+module.exports = {
+	mode: 'development',
+	entry: {
+		bundle: path.resolve(__dirname,'./src/index.js')
+	},
+	output: {
+		path: path.resolve(__dirname,'dist'),
+		filename: '[name][contenthash].js',
+		clean:true,
+		assetModuleFilename: '[name][ext]',
+	},
+	devtool: 'source-map',
 
+	devServer: {
+		static: {
+			directory:path.resolve(__dirname,'dist')
+		},
+		port:3000,
+		open:true,
+		hot: true,
+		compress:true,
+		historyApiFallback: true,
+	},
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env'
+						]
+					}
+				}
+			},
+			{
+				test: /\.(png|svg|jpe?g|gif)$/i,
+				type: 'asset/resource',
+				generator: {
+				    filename: 'assets/[name][contenthash][ext][query]'
+				}
+			},
+		]
+	},
 
-
-
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        new MiniCssExtractPlugin(),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, 'css-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+		}),
+		new BundleAnalyzerPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name][contenthash].css',
+		}),
+	]
 };
