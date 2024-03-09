651a557c055327756ecb9f62d282fc79e724ae54
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-09 17:46:53 -0500

    Put all global variables in the NoteElements array . Attempt to imrpove the code by deleting confusing and unnecessary parts.

--- src/modules/note.js
+++ src/modules/note.js
@@ -1,36 +1,39 @@
+let noteElements = {
+	note: document.querySelector("#note"),
+	inputDate: document.querySelector(".pickDate"),
+	noteContent: document.getElementById("noteContent"),
+	blackboard: document.getElementById("blackboard"),
+	unsortedList : document.getElementById("ul"),
+	noteName: document.getElementById("noteName"),
+	main: document.querySelector("#main"),
+	addBtn: document.getElementById("add"),
+	delBtn: document.getElementById('delete')
+}
+
+
 
 export function addNote(){
 
-let note = document.getElementById("note");
-let inputDate = document.querySelector(".pickDate")
-let noteContent = document.getElementById("noteContent")
-let blackboard = document.getElementById("blackboard")
-let unsortedList  = document.getElementById("ul")
-let noteName = document.getElementById("noteName")
-let main = document.querySelector("#main")
-let addBtn = document.getElementById("add");
-
-
 let xPosition = 0;
 let yPosition = 0;
-noteContent.style.opacity = 1;
+noteElements.noteContent.style.opacity = 1;
 
-function newNote(){
-	let noteCopy= note.cloneNode(true);
+function newNote(noteCopy,noteList,newNote,header,noteContent,label,addDate,textArea,btnBox,addImg,deleteImg){
+	 noteCopy= noteElements.note.cloneNode(true);
 
 	//main elements
-	let noteList = document.createElement("li")
-	let newNote = document.createElement("div");
-	let header = document.createElement("h1");
-	let noteContent = document.createElement("div");
-	let label = document.createElement("label");
-	let addDate = document.createElement("input");
-	let textArea = document.createElement("textarea");
-	let btnBox = document.createElement("div");
-	let addImg = document.createElement("img");
-	let deleteImg = document.createElement("img");
+	 noteList = document.createElement("li")
+	 newNote = document.createElement("div");
+	 header = document.createElement("h1");
+	 noteContent = document.createElement("div");
+	 label = document.createElement("label");
+	 addDate = document.createElement("input");
+	 textArea = document.createElement("textarea");
+	 btnBox = document.createElement("div");
+	 addImg = document.createElement("img");
+	 deleteImg = document.createElement("img");
 
-	header.textContent = noteName.value;
+	header.textContent = noteElements.noteName.value;
 	addImg.src = "./assets/check.svg"
 	deleteImg.src = "./assets/cancel.svg"
 
@@ -50,9 +53,9 @@ function newNote(){
 	noteContent.append(label,addDate,textArea,btnBox);
 	btnBox.append(addImg,deleteImg);
 	newNote.append(header)
-	unsortedList.appendChild(noteList);
+	noteElements.unsortedList.appendChild(noteList);
 
-	unsortedList.appendChild(noteCopy)
+	noteElements.unsortedList.appendChild(noteCopy)
 
 	setTimeout(()=>{
 		noteCopy.remove()
@@ -60,68 +63,144 @@ function newNote(){
 
 	}
 
-	addBtn.addEventListener("click",()=>{
-		if(noteName.value && noteName.value.trim() != ""){
+	// noteElements.addBtn.addEventListener("click",()=>{
+	// 	if(noteElements.noteName.value && noteElements.noteName.value.trim() != ""){
+	// 		newNote()
+	// 	}
+	// })
+
+	// noteElements.delBtn.addEventListener('click', ()=>{
+	// 	noteElements.note.classList.remove('note')
+	// 	noteElements.note.setAttribute('class','noteReturnAnimated')
+	// 	createCoverNote()
+	// 	setInterval(()=>{
+	// 		noteElements.note.classList.remove('noteReturnAnimated')
+	// 		noteElements.note.classList.add('swing')
+	// 	},1900)
+	// })
+
+
+	// function createCoverNote(){
+	// 	if(noteElements.note.classList.contains('swing')){
+	// 		noteElements.note.classList.contains('swing').style.pointerEvents = 'none';
+	// 		noteElements.note.classList.contains('swing').style.userSelect = 'none'
+	// 	}
+	// 	let coverNote = document.createElement('div')
+	// 	coverNote.classList.add('coverNote')
+	// 	noteElements.note.appendChild(coverNote)
+		
+	// 	coverNote.addEventListener('click',noteBack)
+	// }
+
+	// function noteBack(){
+	// 	noteElements.note.classList.remove('swing')
+	// 	noteElements.note.classList.add('noteInit')
+	// 	console.log('Added')
+
+	// }
+
+
+
+
+
+	noteElements.addBtn.addEventListener("click",()=>{
+		if(noteElements.noteName.value && noteElements.noteName.value.trim() != ""){
 			newNote()
 		}
 	})
 
+	noteElements.delBtn.addEventListener('click', ()=>{
+		noteElements.note.classList.remove('note')
+		noteElements.note.classList.remove('noteResize')
+		noteElements.note.setAttribute('class','noteReturnAnimated')
+		createCoverNote()
+		setInterval(()=>{
+			noteElements.note.classList.remove('noteReturnAnimated')
+			noteElements.note.classList.add('swing')
+		},1900)
+	})
 
-let delBtn = document.getElementById('delete')
 
-delBtn.disabled = true;
-
-
-delBtn.addEventListener("click", ()=>{
-	noteReturnAnimation()
-	swingingAnimation()
-})
-
-
-
-function noteReturnAnimation(){
-	main.appendChild(note)
-	// if(!note.classList.contains('noteInit'))
-	note.classList.add('noteReturnAnimated')
-	delBtn.disabled = true;
-
-	setTimeout(()=>{
-		note.classList.remove('noteReturnAnimated')
-		delBtn.disabled = true;
-
-	},2030)
-
-	note.classList.remove("note")
-	note.classList.remove("noteResize")
-}
-
-function swingingAnimation(){
-	setTimeout(()=>{
-		
-		if(!note.classList.contains('note')){
-			note.classList.add('swing')
-			note.addEventListener('click',()=>{
-				if(note.classList.contains('noteReturnAnimated')){
-					initialPosition();
-				};
-			});
-		};
-	},2000);
-}
-
-function initialPosition(){
-	if(note.classList.contains('noteReturnAnimated') && note.classList.contains('swing')){
-		note.classList.remove("swing")
-
-		if(!note.classList.contains('note')){
-			note.classList.add('noteInit')
-			if(note.classList.contains('noteReturnAnimated')){
-				setTimeout(()=>{
-					note.classList.remove('noteInit')
-					},1950)
-			}
-			note.classList.add('note')
-			}
+	function createCoverNote(){
+		if(noteElements.note.classList.contains('swing')){
+			noteElements.note.classList.contains('swing').style.pointerEvents = 'none';
+			noteElements.note.classList.contains('swing').style.userSelect = 'none'
 		}
+		let coverNote = document.createElement('div')
+		coverNote.classList.add('coverNote')
+		noteElements.note.appendChild(coverNote)
+		
+		coverNote.addEventListener('click',()=>{
+			coverNote.remove()
+			noteBack()
+		})
 	}
+
+	function noteBack(){
+
+		noteElements.note.classList.add('noteInit')
+
+		setTimeout(()=>{
+			noteElements.note.classList.remove('noteInit')
+			noteElements.note.classList.remove('swing')
+		},1900)
+		noteElements.note.classList.add('note')
+
+		console.log('Added')
+	}
+
+
+// 	function noteReturnAnimation(){
+// 	note.classList.add('noteReturnAnimated')
+// 	main.appendChild(note)
+// 	// if(!note.classList.contains('noteInit'))
+// 	delBtn.disabled = true;
+
+// 	setTimeout(()=>{
+// 		note.classList.remove('noteReturnAnimated')
+// 		delBtn.disabled = true;
+
+// 	},2030)
+
+// 	note.classList.remove("note")
+// 	note.classList.remove("noteResize")
+// 	console.log('noteReturnAnimation is active')
+
+// }
+
+// function swingingAnimation(){
+// 	setTimeout(()=>{
+// 		if(!note.classList.contains('note')){
+// 			note.classList.add('swing')
+// 		};
+// 	},2000);
+// 	console.log('swingingAnimation is active')
+
+// }
+
+// function initialPosition(){
+// 	if(noteElements.note.classList.contains('noteReturnAnimated') && noteElements.note.classList.contains('swing')){
+// 		noteElements.note.classList.remove("swing")
+// 		console.log('swing is disabled')
+
+
+// 		if(!noteElements.note.classList.contains('note')){
+// 			noteElements.note.classList.add('noteInit')
+// 			console.log('noteinit is added')
+
+// 			if(noteElements.note.classList.contains('noteReturnAnimated')){
+// 				setTimeout(()=>{
+// 					noteElements.note.classList.remove('noteInit')
+// 					},1950)
+// 					console.log('noteInit is deleted')
+
+// 			}
+// 			noteElements.note.classList.add('note')
+// 			console.log('note class is added')
+// 			}
+// 		}
+// 	}
+
+
+	
 }
