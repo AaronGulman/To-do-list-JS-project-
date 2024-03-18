2c1bb5ee31793e96ab7c1710914e92da2cba7b98
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-17 20:21:12 -0500

    Call newProject in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./index.css"
 import { addNoteHandler } from "./modules/note";
+import { newProject } from "./modules/add";
 
 let blackboard = document.querySelector("#blackboard")
 
 
 
 // setInterval(()=>{
+	newProject();
 	addNoteHandler();
 // },1250)
